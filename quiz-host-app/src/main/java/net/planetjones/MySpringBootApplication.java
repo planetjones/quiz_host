f42f1f88ebdb03495120c36f46b112815986132b
Jonathan Jones <jonathan@planetjones.co.uk>
2025-01-05 20:07:10 +0000

    green build

---
+++ quiz-host-app/src/main/java/net/planetjones/MySpringBootApplication.java
@@ -0,0 +1,9 @@
+package net.planetjones;
+
+import org.springframework.boot.SpringApplication;
+
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+}
